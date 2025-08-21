122a99cf65db3e3413ee9bfe5c892cbef2f1f96c
Chan Kang <me@kentakang.com>
2025-08-21 17:42:47 +0900

    feat: add getMendixProjectDirectory util

---
+++ src/utils/getMendixProjectDirectory.ts
@@ -0,0 +1,13 @@
+import path from "path";
+
+import { PROJECT_DIRECTORY } from "../constants";
+import getWidgetPackageJson from "./getWidgetPackageJson";
+
+const getMendixProjectDiectory = async () => {
+  const packageJson = await getWidgetPackageJson();
+  const widgetPath = PROJECT_DIRECTORY;
+
+  return path.join(widgetPath, packageJson.config.projectPath);
+};
+
+export default getMendixProjectDiectory;
