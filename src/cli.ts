190e980e3053d59b0715e4cc65b2d76f963ea6f7
Chan Kang <me@kentakang.com>
2025-08-21 16:14:41 +0900

    fix: remove try-catch on cli

--- src/cli.ts
+++ src/cli.ts
@@ -4,18 +4,12 @@ import { program } from "commander";
 
 import packageJson from "../package.json";
 import buildWebCommand from "./commands/build/web";
-import showMessage from "./utils/showMessage";
-import { COLOR_ERROR } from "./constants";
 
-try {
-  program.version(packageJson.version, '-v, --version', 'display current version');
+program.version(packageJson.version, '-v, --version', 'display current version');
 
-  program
-    .command('build:web')
-    .summary('build web widget')
-    .action(buildWebCommand);
+program
+  .command('build:web')
+  .summary('build web widget')
+  .action(buildWebCommand);
 
-  program.parse();
-} catch (error) {
-  showMessage(`Error occurred: ${COLOR_ERROR((error as Error).message)}`);
-}
+program.parse();
