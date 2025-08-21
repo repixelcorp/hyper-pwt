b259edfaf5017d883163a30974e7548692546b9f
Chan Kang <me@kentakang.com>
2025-08-21 16:44:16 +0900

    feat: add WEB_OUTPUT_DIRECTORY constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,8 @@ export const PROJECT_DIRECTORY = process.cwd();
 
 export const CLI_DIRECTORY = path.join(PROJECT_DIRECTORY, 'node_modules/@repixelcorp/hyper-pwt');
 
+export const WEB_OUTPUT_DIRECTORY = path.join(PROJECT_DIRECTORY, '/dist/tmp/widgets');
+
 export const COLOR_NAME = chalk.bold.blueBright;
 
 export const COLOR_ERROR = chalk.bold.red;
