1577c9302c8467fc4a15a45abd405cc0f3c9d8dc
Chan Kang <me@kentakang.com>
2025-08-21 15:17:24 +0900

    feat: add get widget package json util

--- src/utils/getWidgetVersion.ts
+++ src/utils/getWidgetVersion.ts
@@ -1,23 +1,13 @@
-import fs from 'fs/promises';
-import path from 'path';
-
-import { PROJECT_DIRECTORY } from '../constants';
-import { WidgetPackageJson } from '../types';
+import getWidgetPackageJson from './getWidgetPackageJson';
 
 const getWidgetVersion = async (): Promise<string> => {
-  try {
-    const packageJsonPath = path.join(PROJECT_DIRECTORY, 'package.json');
-    const packageJsonFile = await fs.readFile(packageJsonPath, 'utf-8');
-    const packageJsonData: WidgetPackageJson = JSON.parse(packageJsonFile);
+  const widgetPackageJson = await getWidgetPackageJson();
 
-    if (!packageJsonData.version) {
-      throw new Error('widget version is missing, please check your package.json file.');
-    }
-
-    return packageJsonData.version;
-  } catch (error) {
-    throw new Error('package.json file is not exists.');
+  if (!widgetPackageJson.version) {
+    throw new Error('widget version is missing, please check your package.json file.');
   }
+
+  return widgetPackageJson.version;
 };
 
 export default getWidgetVersion;
