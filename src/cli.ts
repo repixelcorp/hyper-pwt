46eb3bebd4bc92461808a0b7bc0fa2294e2b1889
Chan Kang <me@kentakang.com>
2025-08-21 12:24:15 +0900

    feat: add build command

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,13 @@
-console.log('hello!!');
+import { program } from "commander";
+
+import packageJson from "../package.json";
+import buildCommand from "./commands/build";
+
+program.version(packageJson.version, '-v, --version', 'display current version');
+
+program
+  .command('build')
+  .summary('build widget')
+  .action(buildCommand);
+
+program.parse();
---
+++ src/commands/build/index.ts
@@ -0,0 +1,5 @@
+const buildCommand = () => {
+  
+};
+
+export default buildCommand;
