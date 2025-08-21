db58ddff27bf7dd541a5821228bb238a0d6d72ad
Chan Kang <me@kentakang.com>
2025-08-21 15:03:46 +0900

    feat: add cli error message

--- src/cli.ts
+++ src/cli.ts
@@ -4,12 +4,18 @@ import { program } from "commander";
 
 import packageJson from "../package.json";
 import buildWebCommand from "./commands/build/web";
+import showMessage from "./utils/showMessage";
+import { COLOR_ERROR } from "./constants";
 
-program.version(packageJson.version, '-v, --version', 'display current version');
+try {
+  program.version(packageJson.version, '-v, --version', 'display current version');
 
-program
-  .command('build:web')
-  .summary('build web widget')
-  .action(buildWebCommand);
+  program
+    .command('build:web')
+    .summary('build web widget')
+    .action(buildWebCommand);
 
-program.parse();
+  program.parse();
+} catch (error) {
+  showMessage(`Error occurred: ${COLOR_ERROR((error as Error).message)}`);
+}
