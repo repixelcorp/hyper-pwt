e385ed41d863b2ac4e73c4b5a4a2cff8a1058ff7
Chan Kang <me@kentakang.com>
2025-08-28 14:39:52 +0900

    feat: add type generator

---
+++ src/type-generator/generator.ts
@@ -0,0 +1,146 @@
+import type {
+  WidgetDefinition,
+  Property,
+  PropertyGroup,
+  SystemProperty,
+} from './types';
+import type { GenerateTargetPlatform } from './mendix-types';
+import {
+  mapPropertyTypeToTS,
+  pascalCase,
+  sanitizePropertyKey,
+  formatDescription,
+} from './utils';
+import { getMendixImports, generateMendixImports } from './mendix-types';
+import {
+  extractSystemProperties,
+  hasLabelProperty,
+  generateSystemProps,
+  getSystemPropsImports
+} from './system-props';
+import { generateHeaderComment } from './header';
+
+export function generateTypeDefinition(widget: WidgetDefinition, target: GenerateTargetPlatform): string {
+  const interfaceName = generateInterfaceName(widget.name);
+  const properties = extractAllProperties(widget.properties);
+  const systemProps = extractSystemProperties(widget.properties);
+  const hasLabel = hasLabelProperty(systemProps);
+  const widgetProperties = properties.filter(p => !isSystemProperty(p)) as Property[];
+  
+  let output = '';
+  
+  output += generateHeaderComment();
+
+  const imports = getMendixImports(widgetProperties, target);
+  const systemImports = getSystemPropsImports({ hasLabel, platform: target });
+  const allImports = [...imports, ...systemImports];
+  const importStatements = generateMendixImports(allImports);
+
+  if (importStatements) {
+    output += importStatements + '\n';
+  }
+
+  output += generateJSDoc(widget);
+  output += `export interface ${interfaceName} {\n`;
+
+  const systemPropsLines = generateSystemProps({ hasLabel, platform: target });
+
+  for (const line of systemPropsLines) {
+    output += `  ${line}\n`;
+  }
+
+  if (systemPropsLines.length > 0 && widgetProperties.length > 0) {
+    output += `\n  // Widget properties\n`;
+  }
+
+  for (const property of widgetProperties) {
+    output += generatePropertyDefinition(property, target);
+  }
+
+  output += '}\n';
+
+  return output;
+}
+
+function generateInterfaceName(widgetName: string): string {
+  return `${pascalCase(widgetName)}Props`;
+}
+
+export function extractAllProperties(properties: PropertyGroup[] | Property[]): (Property | SystemProperty)[] {
+  const result: (Property | SystemProperty)[] = [];
+
+  for (const item of properties) {
+    if (isPropertyGroup(item)) {
+      result.push(...item.properties);
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+function isPropertyGroup(item: PropertyGroup | Property | SystemProperty): item is PropertyGroup {
+  return 'caption' in item && 'properties' in item;
+}
+
+function isSystemProperty(item: Property | SystemProperty): item is SystemProperty {
+  return !('type' in item) && 'key' in item;
+}
+
+function generateJSDoc(widget: WidgetDefinition): string {
+  let jsDoc = '/**\n';
+  jsDoc += ` * Props for ${widget.name}\n`;
+  
+  if (widget.description) {
+    jsDoc += ` * ${formatDescription(widget.description)}\n`;
+  }
+
+  if (widget.needsEntityContext) {
+    jsDoc += ` * @needsEntityContext true\n`;
+  }
+
+  if (widget.supportedPlatform && widget.supportedPlatform !== 'Web') {
+    jsDoc += ` * @platform ${widget.supportedPlatform}\n`;
+  }
+
+  jsDoc += ' */\n';
+  return jsDoc;
+}
+
+function generatePropertyDefinition(property: Property, target: GenerateTargetPlatform): string {
+  const indent = '  ';
+  let output = '';
+
+  if (property.description) {
+    output += `${indent}/**\n`;
+    output += `${indent} * ${formatDescription(property.description)}\n`;
+    
+    if (property.caption && property.caption !== property.description) {
+      output += `${indent} * @caption ${property.caption}\n`;
+    }
+
+    if (property.defaultValue !== undefined && property.defaultValue !== '') {
+      output += `${indent} * @default ${property.defaultValue}\n`;
+    }
+
+    if (property.type === 'attribute' && property.attributeTypes) {
+      output += `${indent} * @attributeTypes ${property.attributeTypes.join(', ')}\n`;
+    }
+
+    if (property.type === 'enumeration' && property.enumerationValues) {
+      const values = property.enumerationValues.map(ev => ev.key).join(', ');
+      output += `${indent} * @enum {${values}}\n`;
+    }
+
+    output += `${indent} */\n`;
+  }
+
+  const propertyKey = sanitizePropertyKey(property.key);
+  const optional = property.required === false ? '?' : '';
+  const propertyType = mapPropertyTypeToTS(property, target);
+
+  output += `${indent}${propertyKey}${optional}: ${propertyType};\n`;
+
+  return output;
+}
---
+++ src/type-generator/parser.ts
@@ -0,0 +1,194 @@
+import { XMLParser } from 'fast-xml-parser';
+import type {
+  WidgetDefinition,
+  Property,
+  PropertyGroup,
+  SystemProperty,
+  AttributeType,
+  AssociationType,
+  SelectionType,
+  EnumerationValue,
+  ParsedXMLWidget,
+  ParsedXMLProperty,
+  ParsedXMLPropertyGroup,
+  ParsedXMLSystemProperty,
+  ParsedXMLAttributeType,
+  ParsedXMLAssociationType,
+  ParsedXMLSelectionType,
+  ParsedXMLEnumerationValue,
+} from './types';
+import { ensureArray } from './utils';
+
+const parserOptions = {
+  ignoreAttributes: false,
+  attributeNamePrefix: '',
+  textNodeName: '_',
+  parseAttributeValue: false,
+  trimValues: true,
+  parseTrueNumberOnly: false,
+  parseTagValue: false,
+  allowBooleanAttributes: true,
+};
+
+export function parseWidgetXML(xmlContent: string): WidgetDefinition {
+  const parser = new XMLParser(parserOptions);
+  const parsedXML = parser.parse(xmlContent) as ParsedXMLWidget;
+
+  if (!parsedXML.widget) {
+    throw new Error('Invalid widget XML: missing widget element');
+  }
+
+  const widget = parsedXML.widget;
+
+  const widgetDef: WidgetDefinition = {
+    id: widget.id,
+    name: widget.name,
+    description: widget.description,
+    needsEntityContext: widget.needsEntityContext === 'true',
+    pluginWidget: widget.pluginWidget === 'true',
+    offlineCapable: widget.offlineCapable === 'true',
+    supportedPlatform: (widget.supportedPlatform as 'All' | 'Native' | 'Web') || 'Web',
+    properties: [],
+  };
+
+  if (widget.properties) {
+    widgetDef.properties = parseProperties(widget.properties);
+  }
+
+  return widgetDef;
+}
+
+function parseProperties(props: any): PropertyGroup[] | Property[] {
+  if (props.propertyGroup) {
+    const groups = ensureArray(props.propertyGroup);
+
+    return groups.map(group => parsePropertyGroup(group));
+  }
+
+  const properties: Property[] = [];
+  
+  if (props.property) {
+    const propsArray = ensureArray(props.property);
+
+    for (const prop of propsArray) {
+      properties.push(parseProperty(prop));
+    }
+  }
+
+  return properties;
+}
+
+function parsePropertyGroup(group: ParsedXMLPropertyGroup): PropertyGroup {
+  const properties: (Property | SystemProperty)[] = [];
+
+  if (group.property) {
+    const props = ensureArray(group.property);
+    for (const prop of props) {
+      properties.push(parseProperty(prop));
+    }
+  }
+
+  if (group.systemProperty) {
+    const sysProps = ensureArray(group.systemProperty);
+    for (const sysProp of sysProps) {
+      properties.push(parseSystemProperty(sysProp));
+    }
+  }
+
+  return {
+    caption: group.caption,
+    properties,
+  };
+}
+
+function parseProperty(prop: ParsedXMLProperty): Property {
+  const property: Property = {
+    key: prop.key,
+    type: prop.type,
+    caption: prop.caption || '',
+    description: prop.description || '',
+    required: prop.required !== 'false',
+    isList: prop.isList === 'true',
+  };
+
+  if (prop.defaultValue !== undefined) {
+    property.defaultValue = prop.defaultValue;
+  }
+
+  if (prop.onChange) {
+    property.onChange = prop.onChange;
+  }
+
+  if (prop.dataSource) {
+    property.dataSource = prop.dataSource;
+  }
+
+  if (prop.attributeTypes) {
+    property.attributeTypes = parseAttributeTypes(prop.attributeTypes);
+  }
+
+  if (prop.associationTypes) {
+    property.associationTypes = parseAssociationTypes(prop.associationTypes);
+  }
+
+  if (prop.selectionTypes) {
+    property.selectionTypes = parseSelectionTypes(prop.selectionTypes);
+  }
+
+  if (prop.enumerationValues) {
+    property.enumerationValues = parseEnumerationValues(prop.enumerationValues);
+  }
+
+  if (prop.properties) {
+    const parsedProps = parseProperties(prop.properties);
+    property.properties = parsedProps.filter(p => !('caption' in p && 'properties' in p)) as Property[];
+  }
+
+  if (prop.returnType) {
+    property.returnType = {
+      type: prop.returnType.type as any,
+      isList: prop.returnType.isList === 'true',
+    };
+  }
+
+  return property;
+}
+
+function parseSystemProperty(sysProp: ParsedXMLSystemProperty): SystemProperty {
+  const systemProperty: SystemProperty = {
+    key: sysProp.key,
+  };
+
+  if (sysProp.category) {
+    systemProperty.category = sysProp.category;
+  }
+
+  return systemProperty;
+}
+
+function parseAttributeTypes(attributeTypes: { attributeType: ParsedXMLAttributeType | ParsedXMLAttributeType[] }): AttributeType[] {
+  const types = ensureArray(attributeTypes.attributeType);
+  
+  return types.map(type => type.name);
+}
+
+function parseAssociationTypes(associationTypes: { associationType: ParsedXMLAssociationType | ParsedXMLAssociationType[] }): AssociationType[] {
+  const types = ensureArray(associationTypes.associationType);
+
+  return types.map(type => type.name);
+}
+
+function parseSelectionTypes(selectionTypes: { selectionType: ParsedXMLSelectionType | ParsedXMLSelectionType[] }): SelectionType[] {
+  const types = ensureArray(selectionTypes.selectionType);
+
+  return types.map(type => type.name);
+}
+
+function parseEnumerationValues(enumerationValues: { enumerationValue: ParsedXMLEnumerationValue | ParsedXMLEnumerationValue[] }): EnumerationValue[] {
+  const values = ensureArray(enumerationValues.enumerationValue);
+
+  return values.map(value => ({
+    key: value.key,
+    value: value._ || value.key,
+  }));
+}
